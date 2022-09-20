a0683773b2750c05f117ec5ab88a16c9a314f623
Ryan Hanley <camelCaseGuy@users.noreply.github.com>
2022-09-20 17:02:16 -0500

    brainstorming

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const item = (options) => {
   const description = options.description || 'no description';
   const dueDate = options.dueDate || 'no dueDate';
   const priority = options.priority || 'no priority';
+  const notes = options.notes || 'no notes';
+  const checklist = options.checklist || 'no checklist';
+  return { title, description, dueDate, priority, notes, checklist }
 }
 
 const list = (name) => {
@@ -13,4 +16,29 @@ const list = (name) => {
   const setList = (list) => {
     list = list;
   }
+  return { name };
 }
+
+class Item {
+  constructor(options) {
+    title = options.title || 'no title';
+    description = options.description || 'no description';
+    dueDate = options.dueDate || 'no dueDate';
+    priority = options.priority || 'no priority';
+    notes = options.notes || 'no notes';
+    checklist = options.checklist || 'no checklist';
+  }
+}
+
+class List {
+  constructor(items){
+    this._list = items;
+  }
+  _list = [];
+}
+
+const defaultOptions = () => {
+  
+}
+
+const defaultList = new List();
