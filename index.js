0c097981b684cf6690557ffe5f0a89ce761ed269
Ryan Hanley <camelCaseGuy@users.noreply.github.com>
2022-09-19 22:41:17 -0500

    beginnings of item, list

---
+++ index.js
@@ -0,0 +1,16 @@
+console.log('welcome');
+
+const item = (options) => {
+  const title = options.title || 'no title';
+  const description = options.description || 'no description';
+  const dueDate = options.dueDate || 'no dueDate';
+  const priority = options.priority || 'no priority';
+}
+
+const list = (name) => {
+  const name = name;
+  let list = [];
+  const setList = (list) => {
+    list = list;
+  }
+}
